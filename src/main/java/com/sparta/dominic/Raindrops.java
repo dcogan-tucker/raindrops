82529756f14dfecaf1ac07636116ed4c0ef4b73d
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-30 16:23:36 +0100

    pulled pling, plang and plong values outside the method into static fields

--- src/main/java/com/sparta/dominic/Raindrops.java
+++ src/main/java/com/sparta/dominic/Raindrops.java
@@ -2,19 +2,23 @@ package com.sparta.dominic;
 
 public class Raindrops {
 
+    private static final int PLING_VALUE = 3;
+    private static final int PLANG_VALUE = 5;
+    private static final int PLONG_VALUE = 7;
+
     public String plingPlangPlong(int n) {
         // String builder used to generate result.
         StringBuilder stringBuilder = new StringBuilder();
-        // Check if 3 is a factor of n
-        if (isFactorOf(3, n)) {
+        // Check if the pling value factor is a factor of n
+        if (isFactorOf(PLING_VALUE, n)) {
             stringBuilder.append("Pling");
         }
-        // Check if 5 is a factor of n
-        if (isFactorOf(5, n)) {
+        // Check if plang value is a factor of n
+        if (isFactorOf(PLANG_VALUE, n)) {
             stringBuilder.append("Plang");
         }
-        // Check if 7 is a factor of n
-        if (isFactorOf(7, n)) {
+        // Check if plong value is a factor of n
+        if (isFactorOf(PLONG_VALUE, n)) {
             stringBuilder.append("Plong");
         }
         // If empty none of 3, 5 and 7 are factors.
