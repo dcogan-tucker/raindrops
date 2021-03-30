634b58c91decab2ff0a9e98d5e4c72057703bf00
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-30 15:45:02 +0100

    added tests for outputs for each possible configuration of the words pling, plang and plong

--- src/test/java/com/sparta/dominic/RaindropsTest.java
+++ src/test/java/com/sparta/dominic/RaindropsTest.java
@@ -1,6 +1,59 @@
 package com.sparta.dominic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RaindropsTest {
 
-    private Raindrops raindrops;
+    private Raindrops raindrops = new Raindrops();
+
+    @Test
+    void input3RetunrsPling() {
+        String actual = raindrops.plingPlangPlong(3);
+        String expected = "Pling";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input5ReturnsPlang() {
+        String actual = raindrops.plingPlangPlong(5);
+        String expected = "Plang";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input7ReturnsPlong() {
+        String actual = raindrops.plingPlangPlong(7);
+        String expected = "Plong";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input15ReturnsPlingPlang() {
+        String actual = raindrops.plingPlangPlong(15);
+        String expected = "PlingPlang";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input21ReturnsPlingPlong() {
+        String actual = raindrops.plingPlangPlong(21);
+        String expected = "PlingPlong";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input35ReturnsPlangPlong() {
+        String actual = raindrops.plingPlangPlong(35);
+        String expected = "PlangPlong";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void input105ReturnsPlingPlangPlong() {
+        String actual = raindrops.plingPlangPlong(105);
+        String expected = "PlingPlangPlong";
+        assertEquals(expected, actual);
+    }
 }
