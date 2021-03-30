566688450244dea2685c813b00c435b8e93f31cb
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-30 15:55:24 +0100

    added condition for pling

--- src/main/java/com/sparta/dominic/Raindrops.java
+++ src/main/java/com/sparta/dominic/Raindrops.java
@@ -1,4 +1,11 @@
 package com.sparta.dominic;
 
 public class Raindrops {
+
+    public String plingPlangPlong(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (n % 3 == 0) {
+            stringBuilder.append("Pling");
+        }
+    }
 }
